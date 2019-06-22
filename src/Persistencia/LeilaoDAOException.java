ae126ad4c84962e77b82ecff7fd801ec1bdb057a
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2019-06-22 17:12:20 -0300

    Create DAOExceptions

---
+++ src/Persistencia/LeilaoDAOException.java
@@ -0,0 +1,8 @@
+package Persistencia;
+
+public class LeilaoDAOException extends Exception{
+
+    public LeilaoDAOException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/Persistencia/UsuarioDAOException.java
@@ -0,0 +1,8 @@
+package Persistencia;
+
+public class UsuarioDAOException extends Exception{
+
+    public UsuarioDAOException(String mensagem) {
+        super(mensagem);
+    }
+}
